bf797868cc224ca247d8d5086299b75fb226c46d
LEONARDO MICELLI <leonardomicelli@gmail.com>
2025-08-14 20:51:03 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.2.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+dependencies {
+    implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
+
+    // KTOR DEPENDENCIES
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // WHDT DEPENDENCIES
+    implementation("com.github.whdt:whdt-core:0.4.0")
+
+    // GENAI DEPENDENCIES
+    implementation("com.google.genai:genai-java:1.11.0")
+}
+
+kotlin {
+    jvmToolchain(23)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "llm-service"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
