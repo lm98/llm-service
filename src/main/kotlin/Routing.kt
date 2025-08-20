cecd949e46e02d022669d99fd2949c02532d7efb
lm98 <leonardomicelli@gmail.com>
2025-08-20 12:14:08 +0200

    feat: add llm interaction

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,13 +1,28 @@
 package com.example
 
+import com.google.genai.Client
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.runBlocking
 
 fun Application.configureRouting() {
+    val modelVersion = "gemini-2.5-flash"
+    val llmClient = Client.builder().apiKey(dotenv()["GOOGLE_API_KEY"]).build()
+
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val response = llmClient
+                .async
+                .models
+                .generateContent(modelVersion, "Say hello", null)
+
+            response.thenAccept {
+                runBlocking {
+                    call.respondText(it.text()!!)
+                }
+            }.join()
         }
     }
 }
