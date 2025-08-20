f01efaab1727eb4c7e14d87f6bfcc6d149d6f964
lm98 <leonardomicelli@gmail.com>
2025-08-20 12:53:52 +0200

    feat: add logic to extract mood from http request

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -2,6 +2,8 @@ package com.example
 
 import com.google.genai.Client
 import io.github.cdimascio.dotenv.dotenv
+import io.github.lm98.whdt.core.hdt.model.property.Properties.mood
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -12,17 +14,42 @@ fun Application.configureRouting() {
     val llmClient = Client.builder().apiKey(dotenv()["GOOGLE_API_KEY"]).build()
 
     routing {
-        get("/") {
+        get("api/hdt/{id}/update/mood/{systolic}/{diastolic}") {
+
+            val dtId = call.parameters["id"] ?: return@get call.respondText(
+                "Missing Digital Twin ID", status = HttpStatusCode.BadRequest
+            )
+
+            val systolic = call.parameters["systolic"] ?: return@get call.respondText(
+                "Missing Systolic Blood Pressure", status = HttpStatusCode.BadRequest
+            )
+
+            val diastolic = call.parameters["diastolic"] ?: return@get call.respondText(
+                "Missing Diastolic Blood Pressure", status = HttpStatusCode.BadRequest
+            )
+
+            val prompt = getPrompt(systolic.toInt(), diastolic.toInt())
+
             val response = llmClient
                 .async
                 .models
-                .generateContent(modelVersion, "Say hello", null)
+                .generateContent(modelVersion, prompt, null)
 
-            response.thenAccept {
+            response.thenAccept { res ->
+                val raw = res.text()
+                val mood = mood(raw!!.toInt())
                 runBlocking {
-                    call.respondText(it.text()!!)
+                    call.respond(mood)
                 }
             }.join()
         }
     }
+}
+
+fun getPrompt(systolic: Int, diastolic: Int): String {
+    return """
+        Respond only with a integer numerical value from 0 to 100. 
+        What is my current mood score given a systolic blood pressure of $systolic and a diastolic 
+        blood pressure of $diastolic ?
+    """.trimIndent()
 }
