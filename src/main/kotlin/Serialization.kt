6f5f6520349b9ecd6762e4ed51f7200140fd1f2f
lm98 <leonardomicelli@gmail.com>
2025-08-20 12:44:12 +0200

    feat: configure correct serialization

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -1,11 +1,12 @@
 package com.example
 
+import io.github.lm98.whdt.core.serde.Stub
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Stub.hdtJson)
     }
 }
